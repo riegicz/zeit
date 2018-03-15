906e79642cce9a6e07ecfd70f6e269922126b64d
Petros <theofilisp@gmail.com>
2018-03-15 12:29:28 +0100

    Header and footer added

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  @Input() title: string;
+/*
+  goToProfile = function () {
+    this.router.navigateByUrl('/profile');
+  };
+*/
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
