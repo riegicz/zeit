07ec9ca0adaf4267e61f40b113dddcc6ff636ba3
Petros <theofilisp@gmail.com>
2018-03-15 09:36:28 +0100

    Show generic dates in current month

--- src/app/components/input-month/input-month.component.ts
+++ src/app/components/input-month/input-month.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-input-month',
@@ -11,5 +12,23 @@ export class InputMonthComponent implements OnInit {
 
   ngOnInit() {
   }
+  daysInMonth(): Array<any> {
+    var currentYear = new Date().getFullYear();
+    var currentMonth = new Date().getMonth();
+    var date = new Date(currentYear, currentMonth, 1);
+    var days: Array<any> = [];
+    while (date.getMonth() === currentMonth) {
+      var weekDayName =  moment(date).format('dddd');
+      var monthName =  moment(date).format('LL');
+      days.push({
+        date: monthName,
+        dayOfWeek: weekDayName
+      });
+      //days.push(moment(new Date(date)).format('MMMM / YYYY'));
+      date.setDate(date.getDate() + 1);
+    }
+    return days;
+  }
 
+  allDays = this.daysInMonth();
 }
